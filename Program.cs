1b921746e096d39f155867fdf45fc416730341f2
Edwin <Edwin@DESKTOP-LMJ3198>
2022-07-25 11:08:28 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2_Primer_Proyecto_Visualcshart_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
