eaa5f213b06207ff4ca15d5e55c528ccfde2765d
Edwin <usss017222@ugb.edu.sv>
2022-07-25 11:17:33 -0600

    proyecto inicial

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2_Primer_Proyecto_Visualcshart_Consola
+{
+    class Class1
+    {
+    }
+}
